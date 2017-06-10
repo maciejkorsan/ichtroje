18279f3a19758f873de8457f456eec88bcdaba2f
wg <w.gieniusz@i-g.pl>
2017-06-10 15:25:42 +0200

    w

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,32 @@
 /* main js file */ 
+var $=require('jquery');
 
 
-console.log('hi');
+$(document).ready(function(){
+
+    var add_top = $('[data-js="add"]').offset();
+    var el = $('[data-js="link"]');
+
+
+
+    window.onscroll = function(){
+
+        var sTop = $(window).scrollTop();
+
+        console.log(sTop > add_top.top );
+        if (sTop > add_top.top){
+            el.removeClass('navigation__link--hidden');
+        }else{
+            el.addClass('navigation__link--hidden');
+        }
+
+    }
+
+
+
+
+});
+
+
+
  
